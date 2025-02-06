7f71365e8a29206e765b686ace0ebe2265186bbc
Sachin Jakhar <mesachinjakhar@gmail.com>
2025-02-06 12:16:07 +0530

    ttl removed

--- worker/emailWorker.js
+++ worker/emailWorker.js
@@ -16,10 +16,6 @@ function startWorker() {
             ? process.env.REDIS_URL_PUBLIC
             : process.env.REDIS_URL_INTERNAL,
       },
-      removeOnComplete: true, // Auto-delete completed jobs
-      removeOnFail: {
-        age: 300, // Keep failed jobs for 24 hours (optional)
-      },
     }
   );
 }
